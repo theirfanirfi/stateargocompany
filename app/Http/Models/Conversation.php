79667918e01d6f9bd386c71ab7b743f89e3a435a
theirfanirfi <theirfanirfi@gmail.com>
2018-08-06 09:39:07 +0500

    Messaging section started

---
+++ app/Http/Models/Conversation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    //
+
+    protected $table = "conversation";
+    protected $primaryKey = "cid";
+}
---
+++ app/Http/Models/Messages.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Messages extends Model
+{
+    //
+
+    protected $table = "messages";
+    protected $primaryKey = "mid";
+
+}
